092771f5b40b097fd302f02914f20916fd53d2a2
+ <+>
2022-06-08 17:33:22 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName;
+
+function upperCaseCustomerName(){
+    return customerName.UpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer(name){
+bestCustomer = name
+}
+
+const leastFavourityCustomer = 'Juice'
+
+function changeLeastFavoriteCustomer(){
+    leastFavouriteCustomer = "Orange Soda"
+}
+
+
